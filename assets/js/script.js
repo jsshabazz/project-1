ff93c92170f29219a8e2677ac07c3565ed778028
Jamall Shabazz <jsshabazz510@gmail.com>
2021-04-28 18:17:56 -0500

    add api's

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,35 @@
-// fetch("https://prescriptiontrails.org/api/filter/?by=city&city=Albuquerque&offset=0&count=6",{
-// 	"method": "GET",
-//     credentials: 'same-origin'
-	
-// })
-// .then(response => {
-// 	console.log(response);
-// })
-// .catch(err => {
-// 	console.error(err);
-// });
+function mainLogic() {
+	var location = document.getElementById("location"); 
+	location.addEventListener("submit", function(event) {
+		event.preventDefault();
+		var userInputCity = document.getElementById("userinput").value;
+		searchWeather(userInputCity);
+	});
 
-$.ajax({
-    url: 'https://prescriptiontrails.org/api/filter/?by=city&city=Albuquerque&offset=0&count=6',
-    headers: {
-        'Content-Type': 'application/x-www-form-urlencoded'
-    },
-    type: "GET", /* or type:"GET" or type:"PUT" */
-    dataType: "json",
-    data: {
-    },
-    success: function (result) {
-        console.log(result);
-    },
-    error: function () {
-        console.log("error");
-    }
-});
+
+
+function searchTrails(city) {
+	fetch(`https://api.allorigins.win/get?url=${encodeURIComponent('https://prescriptiontrails.org/api/filter/?by=city&city=Albuquerque&offset=0&count=6')}`)
+		.then(response => {
+			if (response.ok) return response.json()
+			throw new Error('Network response was not ok.')
+		})
+		.then(data => console.log(JSON.parse(data.contents)));
+};
+
+
+function searchWeather(city) {
+	fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=9f2843da8657c44c9e4da3ce3f116ee5&units=imperial`)
+		.then(response => {
+			if (response.ok) return response.json();
+			else {
+				throw new Error('Network response was not ok.')
+			};
+		})
+		.then(function (responseJSON) {
+			console.log('success!', responseJSON.main);
+		})
+};
+
+};
+mainLogic();
